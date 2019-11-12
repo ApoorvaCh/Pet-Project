8a526a30a1506eff224fffab3c6841afe774a789
ApoorvaCh <chennuru.apoorva@gmail.com>
2019-11-12 18:39:25 +0530

    Refresh button functionality added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
+import { DashboardService } from '../services/dashboard.service';
 
 @Component({
   selector: 'app-header',
@@ -9,9 +10,17 @@ export class HeaderComponent implements OnInit {
 
   @HostBinding('class.header') get isHeader() { return true; }
   @HostBinding('class.is-personalizable') get isPersonalizable() { return true; }
-  constructor() { }
+  constructor(private myService:DashboardService) { }
 
   ngOnInit() {
+
+  }
+
+  refresh(){
+    this.myService.emptyDashboard();
+    for(let i=0;i<20;i++){
+    this.myService.loadDog();
+    }
   }
 
 }
